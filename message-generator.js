55fd5724bc714026bcc90f75a5d6308701b74bcd
Thomas Kelly-Lieb <thomaskelly-lieb@Thomass-MBP.attlocal.net>
2023-01-21 21:20:52 -0600

    add message generator js file with DATA

---
+++ message-generator.js
@@ -0,0 +1,39 @@
+/* 
+TOPIC - bonsai style to try
+INPUT - none (just produce a style when program is run. may update with inputs on form in HTML later)
+OUTPUT - tree style, tree size, type (broadleaf evergreen, broadleaf deciduous, coniferous evergreen, coniferous deciduous)
+DATA
+an object with style, size, and type keys.  they contain arrays of 4 different versions to choose from
+FUNCITONS
+1. random number generator
+    -- to choose style size and type
+    -- should have 4 each for simplicity
+2. output statment constructor
+    -- use random function to pick from each of the three categories
+    -- combines selections into output statement describing the chosen bonsai
+*/
+
+const DATA = 
+{
+  'type': ['broadleaf evergreen', 'broadleaf deciduous', 'coniferous evergreen', 'coniferous deciduous'],
+  'style': ['Bunjingi', 'Sokan', 'Kengai', 'Moyogi'],
+  'size': ['mame', 'shohin', 'kifu sho', 'chu']
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
